a5fb2526eded1cff294f3431c822e5a0b2053124
hushaofei <858518145@qq.com>
2016-05-13 18:08:26 +0800

    初始化文件

---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php
+namespace Huy;
+
+class Test
+{
+  public function sayHi()
+  {
+    echo "hi~" . PHP_EOL;
+  }
+}
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+require 'vendor/autoload.php';
+
+$test = new Huy\Test();
+$test->sayHi();
